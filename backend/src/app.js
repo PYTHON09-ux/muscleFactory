a17474eb10eb57002d1d79d074788b77e0d7b68f
PYTHON09-ux <itispavan24@gmail.com>
2025-07-01 16:02:05 +0530

    feat: configure dotenv and add Clerk middleware for authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/config.js';
+import { clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
+import {requireRole} from './middlewares/auth.middleware.js';
 
 
 // Importing necessary modules and configuring environment variables    
@@ -12,6 +14,10 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(clerkMiddleware({
+    secretKey: process.env.CLERK_SECRET_KEY
+}));
+
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true })); 
 app.use(express.static('public'));
@@ -19,14 +25,16 @@ app.use(cookieParser());
 
 // import all routes here 
 
+import userRoutes from './routes/user.routes.js';
+import errorRoute from './routes/error.route.js';
 
 
 
 
 //declare routes here 
 
-
-
+app.use('/api/v1/users',requireAuth(), requireRole('admin'), userRoutes);
+app.use('/api/v1/error', errorRoute);
 export {app};
 // This is the main application file for the backend.
 // It sets up the Express application and configures middleware for parsing JSON and URL-encoded data
